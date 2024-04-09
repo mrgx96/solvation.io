4ea1ab98e82c4b7954ef01e7802fe2dc6af4784c
dragon-8800 <159820590+dragon-8800@users.noreply.github.com>
2024-04-09 04:28:52 -0700

    add pie chart for supply

--- src/sections/Supply.tsx
+++ src/sections/Supply.tsx
@@ -1,5 +1,6 @@
 import SupplyDetails from './SupplyDetails';
 import style from './supply.module.css';
+import pieChart from '../assets/pie-chart.svg';
 
 export default function Supply() {
   return (
@@ -7,6 +8,9 @@ export default function Supply() {
       <div className="container">
         <div className={style.title}>Solvenomics</div>
         <div className={style.totalSupply}>Total Supply: 3,333,333,333</div>
+        <div className={style.pieChart}>
+          <img src={pieChart} alt="Pie Chart" />
+        </div>
         <SupplyDetails />
       </div>
     </section>
